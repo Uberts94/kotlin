f2fb79adacda15653bf70f8eeac9290dd101a790
pietroubertini <s292522@studenti.polito.it>
2022-03-15 19:15:14 +0100

    Constructor Implemented

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,5 +1,4 @@
 data class MinesweeperBoard(val board: List<String>) {
-    // TODO: Implement proper constructor
     fun withNumbers(): List<String> {
         TODO("Implement this function to complete the task")
     }
