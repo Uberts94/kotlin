38209c9e16ce3b2eabf1b5d6ec7ffc0aa3ed50d6
Pietro Ubertini <79876343+Uberts94@users.noreply.github.com>
2022-03-15 12:15:33 +0100

    Update Minesweeper.kt

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,7 +1,6 @@
 data class MinesweeperBoard(val todo: Nothing) {
 
     // TODO: Implement proper constructor
-    //prova di push su github
     fun withNumbers(): List<String> {
         TODO("Implement this function to complete the task")
     }
